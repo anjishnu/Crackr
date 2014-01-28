a85e9ceba7b6719dcf8e1b94448aa03fda5e7365
Anjishnu Kumar <anjishnu@ubuntu.(none)>
2014-01-28 01:30:49 -0500

    Updated CRAKR to the latest version and added README file

---
+++ webserver/postagger.py
@@ -0,0 +1,25 @@
+from nltk.tag.stanford import POSTagger
+import textprocess as tp
+import os, time
+
+#Wraps the part of speech taggin functionality within this file
+
+try:
+    pwd = os.path.dirname(os.path.realpath(__file__))
+    print pwd
+except:
+    print 'Something screwed up, using os.getcwd() instead'
+    pwd = os.getcwd()
+    
+print "POSTagger Loaded"
+post = POSTagger(pwd+'/stanford-postagger/models/english-bidirectional-distsim.tagger',
+                 pwd+"/stanford-postagger/stanford-postagger.jar")
+
+def tag(text):
+    text = tp.preprocess(text)
+    #print text
+    t1 = time.time()
+    outlist = post.tag(text.split())
+    t2 = time.time()
+    print "POS Tagging complete. Time taken: ", t2-t1, " seconds"
+    return outlist
